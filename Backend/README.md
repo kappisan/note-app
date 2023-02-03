154ccd82e6a90e07babb6220686be13f2d2ceeb3
Kappisan <wilkosz.ian@gmail.com>
2023-02-03 15:20:23 +0000

    Add create user and login endpoints

--- Backend/README.md
+++ Backend/README.md
@@ -1,6 +1,15 @@
 # API
 This server uses the Express framework to handle incoming HTTP requests and provide RESTful API endpoints for interacting with the SQLite database. The /getNotes endpoint returns all notes stored in the database, the /createNote endpoint creates a new note, and the /editNote endpoint edits an existing note.
 
+# NodeJS
+
+Endpoints:
+
+>/getNotes
+>/createNote
+>/editNote
+
+
 # SQL
 
 To set up the SQLite database used in the backend, you need to run the following commands in the terminal:
@@ -19,20 +28,20 @@ python
 
 >.open notes.db
 
-    Create a table for storing notes, with columns for the ID and task:
+Create a table for storing notes, with columns for the ID and task:
 
 sql
 
 >CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT NOT NULL);
 
-    Insert some data into the notes table:
+Insert some data into the notes table:
 
 sql
 
 >INSERT INTO notes (task) VALUES ('Write code');
 >INSERT INTO notes (task) VALUES ('Go for a run');
 
-    Query the data in the notes table:
+Query the data in the notes table:
 
 sql
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 3000;
+const secretKey = 'yoursecretkey';
 
 const db = new sqlite3.Database(':memory:', (err) => {
   if (err) {
@@ -11,61 +13,140 @@ const db = new sqlite3.Database(':memory:', (err) => {
   console.log('Connected to the in-memory SQlite database.');
 });
 
-// Create table for storing notes
+// Create tables for storing users and notes
 db.run(
-  'CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT NOT NULL)'
+  'CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL)'
+);
+db.run(
+  'CREATE TABLE notes (id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, task TEXT NOT NULL, FOREIGN KEY (user_id) REFERENCES users(id))'
 );
 
 app.use(express.json());
 
-// Get all notes
-app.get('/getNotes', (req, res) => {
-  db.all('SELECT * FROM notes', (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: 'Failed to fetch notes' });
-    }
-    res.json(rows);
-  });
-});
-
-// Create a new note
-app.post('/createNote', (req, res) => {
-  const task = req.body.task;
-  if (!task) {
-    return res.status(400).json({ error: 'Task is required' });
+// Create a new user
+app.post('/createUser', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
   }
   db.run(
-    'INSERT INTO notes (task) VALUES (?)',
-    task,
+    'INSERT INTO users (username, password) VALUES (?, ?)',
+    username,
+    password,
     function (err) {
       if (err) {
-        res.status(500).json({ error: 'Failed to create note' });
+        res.status(500).json({ error: 'Failed to create user' });
       }
-      res.json({ id: this.lastID, task });
+      res.json({ id: this.lastID, username });
     }
   );
+});
+
+// Login a user
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required' });
+  }
+  db.get(
+    'SELECT * FROM users WHERE username = ? AND password = ?',
+    username,
+    password,
+    (err, row) => {
+      if (err) {
+        res.status(500).json({ error: 'Failed to fetch user' });
+      }
+      if (!row) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+      }
+      const token = jwt.sign({ id: row.id }, secretKey, { expiresIn: '1h' });
+      res.json({ token });
+    }
+  );
+});
+
+
+// Get all notes for a user
+app.get('/getNotes', (req, res) => {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ error: 'Access token is required' });
+    }
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Invalid access token' });
+      }
+      const userId = decoded.id;
+      db.all('SELECT * FROM notes WHERE user_id = ?', userId, (err, rows) => {
+        if (err) {
+          return res.status(500).json({ error: 'Failed to fetch notes' });
+        }
+        res.json(rows);
+      });
+    });
+  });
+  
+// Create a new note
+app.post('/createNote', (req, res) => {
+const token = req.headers.authorization;
+if (!token) {
+    return res.status(401).json({ error: 'Access token is required' });
+}
+jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+    return res.status(401).json({ error: 'Invalid access token' });
+    }
+    const userId = decoded.id;
+    const { task } = req.body;
+    if (!task) {
+    return res.status(400).json({ error: 'Task is required' });
+    }
+    db.run(
+    'INSERT INTO notes (user_id, task) VALUES (?, ?)',
+    userId,
+    task,
+    function (err) {
+        if (err) {
+        res.status(500).json({ error: 'Failed to create note' });
+        }
+        res.json({ id: this.lastID, task });
+    }
+    );
+});
 });
 
 // Edit a note
-app.post('/editNote', (req, res) => {
-  const id = req.body.id;
-  const task = req.body.task;
-  if (!id || !task) {
-    return res
-      .status(400)
-      .json({ error: 'Note ID and task are both required' });
-  }
-  db.run(
-    'UPDATE notes SET task = ? WHERE id = ?',
-    task,
-    id,
-    function (err) {
-      if (err) {
-        res.status(500).json({ error: 'Failed to edit note' });
-      }
-      res.json({ id, task });
+app.put('/editNote/:id', (req, res) => {
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ error: 'Access token is required' });
     }
-  );
+    jwt.verify(token, secretKey, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Invalid access token' });
+      }
+      const userId = decoded.id;
+      const noteId = req.params.id;
+      const { task } = req.body;
+      if (!task) {
+        return res.status(400).json({ error: 'Task is required' });
+      }
+      db.run(
+        'UPDATE notes SET task = ? WHERE id = ? AND user_id = ?',
+        task,
+        noteId,
+        userId,
+        function (err) {
+          if (err) {
+            return res.status(500).json({ error: 'Failed to update note' });
+          }
+          if (this.changes === 0) {
+            return res.status(404).json({ error: 'Note not found' });
+          }
+          res.json({ id: noteId, task });
+        }
+      );
+    });
 });
 
 app.listen(port, () => {
