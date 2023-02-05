08664a8f6496c1b0865ab1ef70a1230ea6f48fb9
Kappisan <wilkosz.ian@gmail.com>
2023-02-05 01:52:28 +0000

    Add bootstrap and use a modal to edit tasks

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,36 @@
-import logo from './logo.svg';
 import React, { useState } from 'react';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+// import TaskModal from './components/TaskModal/TaskModal.tsx';
+
+
 import './App.css';
 
 const TodoApp = () => {
+
+  const today = new Date().toDateString();
+  const [show, setShow] = useState(false);
+  const [todo, setTodo] = useState({});
+  
+  const showEdit = (todo) => {
+    console.log("handle show", todo);
+    setTodo(todo);
+    setShow(true);
+  }
+  
+
+  const closeEdit = () => setShow(false);
+
+  // will be empty
   const [todos, setTodos] = useState([
-    { id: 1, task: 'Do laundry', completed: false },
-    { id: 2, task: 'Buy groceries', completed: false },
+    { id: 1, added: today, done: true, task: 'Do laundry', completed: false },
+    { id: 2, added: today, done: false, task: 'Buy groceries', completed: false },
   ]);
 
   const addTodo = (task) => {
-    setTodos([...todos, { id: todos.length + 1, task, completed: false }]);
+    setTodos([...todos, { id: todos.length + 1, task, completed: false, added: new Date().toDateString() }]);
   };
 
   const editTodo = (id, task) => {
@@ -21,19 +42,52 @@ const TodoApp = () => {
   };
 
   return (
-    <div>
-      <h1>To-Do App</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.task}{' '}
-            <button onClick={() => editTodo(todo.id, prompt('Edit Todo:', todo.task))}>
-              Edit
-            </button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={() => addTodo(prompt('Add Todo:'))}>Add Todo</button>
+    <div className="gradient">
+      <div className="container">
+
+        <Modal show={show} onHide={closeEdit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{todo.task}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeEdit}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={() => { /*editTodo(todo.id, prompt('Edit Todo:', todo.task));*/ }}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+
+        <h1>To-Do List</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Done</th>
+              <th>Added</th>
+              <th>Task</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr key={todo.id}>
+                <td><input type='checkbox' value={todo.done} /></td>
+                <td>{todo.added}</td>
+                <td>{todo.task}</td>
+                <td>
+                  <button type="button" className="btn btn-primary" onClick={ () => showEdit(todo) }>
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button type="button" className="btn btn-primary" onClick={() => addTodo(prompt('Add Todo:'))}>Add Todo</button>
+      </div>
     </div>
   );
 };
